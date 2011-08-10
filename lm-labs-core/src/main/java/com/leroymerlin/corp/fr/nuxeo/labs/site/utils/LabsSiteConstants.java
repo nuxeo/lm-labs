74884d5bad25c96b29655255ec303e5776e5011b
Vincent Dutat <vincent.dutat@ext.leroymerlin.fr>
2011-08-10 15:13:35 +0200

    typo

--- lm-labs-core/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/utils/LabsSiteConstants.java
+++ lm-labs-core/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/utils/LabsSiteConstants.java
@@ -8,7 +8,7 @@ import org.apache.commons.lang.StringUtils;
 public final class LabsSiteConstants {
     
     public enum Docs {
-        DEFAULT_DOMAIN("DefaultDomain", "default-domain"),
+        DEFAULT_DOMAIN("Domain", "default-domain"),
         SITESROOT("SitesRoot", "sites"),
         SITE("Site", StringUtils.EMPTY),
         TREE("Tree", "tree"),
