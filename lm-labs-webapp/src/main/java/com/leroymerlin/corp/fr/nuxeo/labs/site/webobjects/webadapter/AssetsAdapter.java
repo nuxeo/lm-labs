6b4f7474d01557c931f809754a60907ad559981a
Eric Phongsavanh <eric.phongsavanh@leroymerlin.fr>
2011-09-13 11:58:55 +0200

    correct path

--- lm-labs-webapp/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/webobjects/webadapter/AssetsAdapter.java
+++ lm-labs-webapp/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/webobjects/webadapter/AssetsAdapter.java
@@ -11,22 +11,25 @@ import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.rest.DocumentObject;
 import org.nuxeo.ecm.webengine.WebException;
 import org.nuxeo.ecm.webengine.model.WebAdapter;
 import org.nuxeo.ecm.webengine.model.impl.DefaultAdapter;
 
 import com.leroymerlin.corp.fr.nuxeo.labs.site.utils.LabsSiteConstants;
 
-@WebAdapter(name = "assets", type = "assetsAdapter")
+@WebAdapter(name = "assets", type = "assetsAdapter", targetType = "LabsSite")
 public class AssetsAdapter extends DefaultAdapter {
 
     private static final Log LOG = LogFactory.getLog(AssetsAdapter.class);
 
     public DocumentModel getAssetDoc() {
         try {
-            PathRef ref = new PathRef("/" + LabsSiteConstants.Docs.DEFAULT_DOMAIN.docName()
-                    + "/" + LabsSiteConstants.Docs.SITESROOT.docName() + "/"
-                    + this.getPrevious().getName() + "/"
+            DocumentObject site = (DocumentObject) this.getTarget();
+            PathRef ref = new PathRef("/"
+                    + LabsSiteConstants.Docs.DEFAULT_DOMAIN.docName() + "/"
+                    + LabsSiteConstants.Docs.SITESROOT.docName() + "/"
+                    + site.getDocument().getName() + "/"
                     + LabsSiteConstants.Docs.ASSETS.docName());
             return ctx.getCoreSession().getDocument(ref);
         } catch (ClientException e) {
