bfd5c10f033fb5cb85f0c109b2d60f356daea9ed
Vincent Dutat <vincent.dutat@ext.leroymerlin.fr>
2011-12-01 12:47:11 +0100

    fix 'external URLs' area - remove field 'order' from schema & adapter

--- lm-labs-core/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/blocs/ExternalURLAdapter.java
+++ lm-labs-core/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/blocs/ExternalURLAdapter.java
@@ -1,8 +1,5 @@
 package com.leroymerlin.corp.fr.nuxeo.labs.site.blocs;
 
-import java.io.Serializable;
-
-import org.apache.commons.lang.StringUtils;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
 
@@ -10,7 +7,6 @@ public class ExternalURLAdapter implements ExternalURL {
     
     static final String NAME = "exturl:name";
     static final String URL = "exturl:url";
-    static final String ORDER = "exturl:order";
     
     protected DocumentModel doc;
 
@@ -39,21 +35,8 @@ public class ExternalURLAdapter implements ExternalURL {
     }
 
     @Override
-    public int getOrder() throws ClientException {
-        Serializable value = doc.getPropertyValue(ORDER);
-        if (value != null && !StringUtils.isEmpty(value.toString())){
-            return Integer.valueOf(value.toString()).intValue();
-        }
-        return -1;
-    }
-
-    @Override
-    public void setOrder(int pOrder) throws ClientException {
-        doc.setPropertyValue(ORDER, pOrder);
-    }
-
-    @Override
     public DocumentModel getDocument() {
         return doc;
     }
+
 }
