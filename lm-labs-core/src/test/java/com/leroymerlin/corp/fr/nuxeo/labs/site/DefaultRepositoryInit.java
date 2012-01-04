323b78ecc002088c09cea6b53fe9374f6a7e2cbe
Vincent Dutat <vincent.dutat@ext.leroymerlin.fr>
2012-01-04 13:19:58 +0100

    use constants

--- lm-labs-core/src/test/java/com/leroymerlin/corp/fr/nuxeo/labs/site/DefaultRepositoryInit.java
+++ lm-labs-core/src/test/java/com/leroymerlin/corp/fr/nuxeo/labs/site/DefaultRepositoryInit.java
@@ -5,13 +5,13 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.test.annotations.RepositoryInit;
 
-import com.leroymerlin.corp.fr.nuxeo.labs.site.utils.LabsSiteConstants;
+import com.leroymerlin.corp.fr.nuxeo.labs.site.utils.LabsSiteConstants.Docs;
 
 public class DefaultRepositoryInit implements RepositoryInit {
 
     @Override
     public void populate(CoreSession session) throws ClientException {
-        DocumentModel doc = session.createDocumentModel("/", "default-domain",LabsSiteConstants.Docs.DEFAULT_DOMAIN.type());
+        DocumentModel doc = session.createDocumentModel("/", Docs.DEFAULT_DOMAIN.docName(),Docs.DEFAULT_DOMAIN.type());
         session.createDocument(doc);
         session.save();
 
