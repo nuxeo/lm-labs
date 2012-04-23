61802f2cfae45e9aa8e5f9c5237522c243ee262d
Ludovic Potrich <ludovic.potrich@ext.leroymerlin.fr>
2012-04-23 11:05:09 +0200

    bug forums

--- lm-forums/lm-forums-core/src/main/java/com/leroymerlin/corp/fr/nuxeo/forum/LMForumAdapterFactory.java
+++ lm-forums/lm-forums-core/src/main/java/com/leroymerlin/corp/fr/nuxeo/forum/LMForumAdapterFactory.java
@@ -3,11 +3,13 @@ package com.leroymerlin.corp.fr.nuxeo.forum;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.adapter.DocumentAdapterFactory;
 
+import com.leroymerlin.corp.fr.nuxeo.labs.site.utils.LabsSiteConstants.Docs;
+
 public class LMForumAdapterFactory implements DocumentAdapterFactory {
 
 	@Override
 	public Object getAdapter(DocumentModel doc, Class<?> klass) {
-		if(doc.getType().equals("LMForum")) {
+		if(doc.getType().equals(Docs.PAGEFORUM.type())) {
 			return new LMForumImpl(doc);
 		}
 		return null;
