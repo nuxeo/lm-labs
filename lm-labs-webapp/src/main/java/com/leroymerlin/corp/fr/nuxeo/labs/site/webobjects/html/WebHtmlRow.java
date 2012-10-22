be56bb5f4206a8c6421c563e8a3747160c4324eb
Fabrice Vandaele <fabrice.vandaele@leroymerlin.fr>
2012-10-22 11:33:06 +0200

    bad library

--- lm-labs-webapp/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/webobjects/html/WebHtmlRow.java
+++ lm-labs-webapp/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/webobjects/html/WebHtmlRow.java
@@ -10,7 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.ClientException;
