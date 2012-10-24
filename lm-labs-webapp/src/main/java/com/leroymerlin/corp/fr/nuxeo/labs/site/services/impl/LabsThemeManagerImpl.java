42b1f770b7abec788b242043c6a6c8b145675010
Sun Seng David TAN <stan@nuxeo.com>
2012-10-24 14:22:47 +0200

    Get list of theme from bundle resources instead of the module file system

--- lm-labs-webapp/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/services/impl/LabsThemeManagerImpl.java
+++ lm-labs-webapp/src/main/java/com/leroymerlin/corp/fr/nuxeo/labs/site/services/impl/LabsThemeManagerImpl.java
@@ -1,19 +1,22 @@
 package com.leroymerlin.corp.fr.nuxeo.labs.site.services.impl;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.runtime.model.ComponentContext;
 import org.nuxeo.runtime.model.DefaultComponent;
-
+import com.leroymerlin.corp.fr.nuxeo.labs.LabsWebAppActivator;
 import com.leroymerlin.corp.fr.nuxeo.labs.site.services.LabsThemeManager;
 import com.leroymerlin.corp.fr.nuxeo.labs.site.utils.DirectoriesUtils;
 import com.leroymerlin.corp.fr.nuxeo.labs.site.utils.LabsSiteConstants.Directories;
 import com.leroymerlin.corp.fr.nuxeo.labs.site.utils.LabsSiteWebAppUtils;
 
 public class LabsThemeManagerImpl extends DefaultComponent implements LabsThemeManager {
-    
+
     @Override
     public void activate(ComponentContext context) throws Exception {
     }
@@ -21,7 +24,21 @@ public class LabsThemeManagerImpl extends DefaultComponent implements LabsThemeM
     @SuppressWarnings("unchecked")
     @Override
     public List<String> getThemeList(String pathBase) {
-        return (List<String>) CollectionUtils.intersection(getDirThemes(), LabsSiteWebAppUtils.getFoldersUnderFolder(pathBase + LabsSiteWebAppUtils.DIRECTORY_THEME));
+        Enumeration<URL> entries = LabsWebAppActivator.getDefault().getBundle().findEntries(
+                LabsSiteWebAppUtils.DIRECTORY_THEME, "specific.less", true);
+
+        List<String> folderList = new ArrayList<String>();
+        while (entries.hasMoreElements()) {
+            URL url = entries.nextElement();
+            String[] nodes = url.getPath().split("/");
+            if (nodes.length > 1) {
+                String lastname = nodes[nodes.length - 2];
+                folderList.add(lastname);
+            }
+        }
+        return (List<String>) CollectionUtils.intersection(getDirThemes(),
+                folderList);
+
     }
 
     @SuppressWarnings("unchecked")
@@ -37,12 +54,12 @@ public class LabsThemeManagerImpl extends DefaultComponent implements LabsThemeM
     public static List<String> getDirPageTemplates() {
         return DirectoriesUtils.getDirList(Directories.PAGE_TEMPLATES);
     }
-    
+
     @Override
     public DocumentModelList getDirFontSizes() {
         return DirectoriesUtils.getDirDocumentModelList(Directories.FONT_SIZES);
     }
-    
+
     @Override
     public DocumentModelList getDirFontFamilies() {
         return DirectoriesUtils.getDirDocumentModelList(Directories.FONT_FAMILIES);
